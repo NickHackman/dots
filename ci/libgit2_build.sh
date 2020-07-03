10b0403b5d933ab17b028f902fdec18be651ac35
NickHackman <snickhackman@gmail.com>
2020-07-03 13:48:49 -0400

    Needed -d flag for go get

--- ci/libgit2_build.sh
+++ ci/libgit2_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-go get github.com/libgit2/git2go 
+go get -d github.com/libgit2/git2go 
 cd "${GOPATH}"/src/github.com/libgit2/git2go || exit 1
 git checkout next
 git submodule update --init # get libgit2
